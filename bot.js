f4fd7715a084e7d43d496c0d337940c38130c574
ibrahim-ih1518019 <46673101+ibrahim-ih1518019@users.noreply.github.com>
2020-12-02 13:11:23 +0300

    hehe

---
+++ bot.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+// partials is for specifying the when i guess
+const client = new Discord.Client({
+  partials: ["MESSAGE"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageDelete", (msg) => {
+  msg.channel.send("Bish i saw u deleting");
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "meow") {
+    msg.reply("Pong!");
+    msg.react("❤");
+  }
+  if (msg.content === "meow-") {
+    msg.reply("Please add a command !");
+  }
+  if (msg.content === "meow-ow") {
+    msg.member.roles.add("783632383445303296");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
